116950339b6a97c41beeb80baaa034a57ec8ea57
Jkorf <jankorf91@gmail.com>
2025-01-22 13:47:45 +0100

    Update HyperLiquidOptions object to make use of LibraryOptions base class

--- HyperLiquid.Net/Objects/Options/HyperLiquidOptions.cs
+++ HyperLiquid.Net/Objects/Options/HyperLiquidOptions.cs
@@ -1,36 +1,12 @@
 using CryptoExchange.Net.Authentication;
-using Microsoft.Extensions.DependencyInjection;
+using CryptoExchange.Net.Objects.Options;
 
 namespace HyperLiquid.Net.Objects.Options
 {
     /// <summary>
     /// HyperLiquid options
     /// </summary>
-    public class HyperLiquidOptions
+    public class HyperLiquidOptions : LibraryOptions<HyperLiquidRestOptions, HyperLiquidSocketOptions, ApiCredentials, HyperLiquidEnvironment>
     {
-        /// <summary>
-        /// Rest client options
-        /// </summary>
-        public HyperLiquidRestOptions Rest { get; set; } = new HyperLiquidRestOptions();
-
-        /// <summary>
-        /// Socket client options
-        /// </summary>
-        public HyperLiquidSocketOptions Socket { get; set; } = new HyperLiquidSocketOptions();
-
-        /// <summary>
-        /// Trade environment. Contains info about URL's to use to connect to the API. Use `HyperLiquidEnvironment` to swap environment, for example `Environment = HyperLiquidEnvironment.Live`
-        /// </summary>
-        public HyperLiquidEnvironment? Environment { get; set; }
-
-        /// <summary>
-        /// The api credentials used for signing requests.
-        /// </summary>
-        public ApiCredentials? ApiCredentials { get; set; }
-
-        /// <summary>
-        /// The DI service lifetime for the IHyperLiquidSocketClient
-        /// </summary>
-        public ServiceLifetime? SocketClientLifeTime { get; set; }
     }
 }
